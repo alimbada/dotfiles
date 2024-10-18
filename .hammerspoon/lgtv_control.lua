11878520aaaae74e8448cc1dbb62e7522103c79a
Ammaar Limbada <ammaar.limbada@madetech.com>
2024-10-18 11:11:48 +0100

    Updated LGTV scripts for Hammerspoon

--- .hammerspoon/lgtv_control.lua
+++ .hammerspoon/lgtv_control.lua
@@ -1,20 +1,11 @@
-local lgtv_path = "~/opt/lgtv/bin/lgtv" -- Full path to lgtv executable
+local lgtv_path = "~/lgtv-venv/bin/lgtv" -- Full path to lgtv executable
 local tv_name = "MyTV" -- Name of your TV, set when you run `lgtv auth`
-local lgtv_cmd = lgtv_path.." "..tv_name
+local lgtv_cmd = lgtv_path.." --ssl --name "..tv_name
 
 local my_input = "HDMI_3"
 local other_input = "HDMI_4"
 
 function exec_command(command)
-    space_loc = command:find(" ")
-
-    --- "ssl" must be the first argument for commands like 'startApp'. Advance it to the expected position.
-    if space_loc then
-        command = command:sub(1, space_loc) .. "ssl " .. command:sub(space_loc + 1)
-    else
-        command = command .. " ssl"
-    end
-
     command = lgtv_cmd .. " " .. command
     -- print("Executing command: " .. command)
 
