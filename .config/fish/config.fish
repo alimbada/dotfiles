2467009c5f7582dec7bae07723f0c4a18d914f73
Ammaar Limbada <alimbada@users.noreply.github.com>
2025-02-20 12:04:10 +0000

    Added zoxide and fzf

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,8 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     starship init fish | source
+    fzf --fish | source
+    zoxide init --cmd cd fish | source
 end
 
 set -gx EDITOR nvim
