df1e2be84a891aa9c2855789e5fb3d01c5e78261
Ammaar Limbada <alimbada@users.noreply.github.com>
2025-02-24 23:23:37 +0000

    fzf config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,10 +1,8 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     starship init fish | source
-    fzf --fish | source
+    set -x FZF_DEFAULT_OPTS '--height 30%  --layout reverse --border top'
     zoxide init --cmd cd fish | source
 end
-
-set -gx EDITOR nvim
 
 source ~/.config/fish/abbr.fish
