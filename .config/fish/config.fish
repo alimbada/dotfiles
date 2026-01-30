66201488703f415075e4084679a6c676f08eb502
Ammaar Limbada <alimbada@users.noreply.github.com>
2026-01-30 16:07:27 +0000

    source fzf

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,6 +4,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     starship init fish | source
     set -x FZF_DEFAULT_OPTS '--height 30%  --layout reverse --border top'
+    fzf --fish | source
     zoxide init --cmd cd fish | source
 end
 
