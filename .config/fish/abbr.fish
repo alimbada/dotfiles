bfa98ef6f72be4f71b23e4f514ece09312da1e43
Ammaar Limbada <alimbada@users.noreply.github.com>
2026-02-09 13:55:40 +0000

    abbr for docker compose

--- .config/fish/abbr.fish
+++ .config/fish/abbr.fish
@@ -3,3 +3,4 @@ abbr -a -- l eza --group-directories-first
 abbr -a -- ll eza -l --group-directories-first
 abbr -a -- la eza -al --group-directories-first
 abbr -a -- tree eza -T
+abbr -a -- dc docker compose
