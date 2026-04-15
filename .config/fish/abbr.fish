cf9d77b72e3341e95265e0e096b4d786885304f9
Ammaar Limbada <alimbada@users.noreply.github.com>
2026-04-15 13:05:00 +0100

    merged abbr from linux branch

--- .config/fish/abbr.fish
+++ .config/fish/abbr.fish
@@ -1,4 +1,6 @@
-abbr -a -- l eza
-abbr -a -- ls eza
-abbr -a -- ll eza -l
-abbr -a -- la eza -al
+abbr -a -- dpss "docker ps --format 'table {{.ID}}\t{{.Names}}\t{{.Status}}'"
+abbr -a -- l eza --group-directories-first
+abbr -a -- ll eza -l --group-directories-first
+abbr -a -- la eza -al --group-directories-first
+abbr -a -- tree eza -T
+abbr -a -- dc docker compose
