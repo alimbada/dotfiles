06e0d65083253ebb32c721abbc2e650f0cddb11c
Ammaar Limbada <alimbada@users.noreply.github.com>
2026-05-22 16:00:27 +0100

    Added ports field to docker ps alias

--- .config/fish/abbr.fish
+++ .config/fish/abbr.fish
@@ -1,4 +1,4 @@
-abbr -a -- dpss "docker ps --format 'table {{.ID}}\t{{.Names}}\t{{.Status}}'"
+abbr -a -- dpss "docker ps --format 'table {{.ID}}\t{{.Names}}\t{{.Status}}\t{{.Ports}}'"
 abbr -a -- l eza --group-directories-first
 abbr -a -- ll eza -l --group-directories-first
 abbr -a -- la eza -al --group-directories-first
