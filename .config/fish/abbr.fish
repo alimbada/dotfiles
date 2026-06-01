8a518d8da52c31708dd4a70a4bdc2f69fa285da4
Ammaar Limbada <alimbada@users.noreply.github.com>
2026-06-01 21:58:54 +0000

    Make bat output without line numbers

--- .config/fish/abbr.fish
+++ .config/fish/abbr.fish
@@ -1,7 +1,7 @@
-abbr -a -- dpss "docker ps --format 'table {{.ID}}\t{{.Names}}\t{{.Status}}'"
+abbr -a -- dpss "docker ps --format 'table {{.ID}}\t{{.Names}}\t{{.Status}}\t{{.Ports}}'"
 abbr -a -- l eza --group-directories-first
 abbr -a -- ll eza -l --group-directories-first
 abbr -a -- la eza -al --group-directories-first
 abbr -a -- tree eza -T
 abbr -a -- dc docker compose
-abbr -a -- bat batcat
+abbr -a -- bat batcat -p
